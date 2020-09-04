04d42f9607abc4cc546a40fc737ac4ab50ba7579
Zane Deupree <zdtexan@gmail.com>
2020-09-04 17:18:46 -0500

    change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./db')
 const movieRouter = require('./routes/movie-router')
 
 const app = express()
-const PORT = process.argv.PORT || 8070
+const PORT = process.env.PORT || 8070
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
