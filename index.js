f3716f28cac3a60244283f1facac3fa32daeff69
Zane Deupree <zdtexan@gmail.com>
2020-09-04 16:51:52 -0500

    hopeful fix

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
 
-const db = require('./db')
-const movieRouter = require('./routes/movie-router')
-
-const app = express()
-const PORT = process.argv.PORT || 8070
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-app.use(express.static("public"))
-
-app.use('/api', movieRouter)
-app.use("*", function(req, res){
-    res.sendFile("./client/build/index.html")
-  })
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://zane:Root1996@cluster0.umq8p.mongodb.net/cluster0?retryWrites=true&w=majority"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
