0f410306a516564502af870b22a2e7ddd07d6405
Zane Deupree <zdtexan@gmail.com>
2020-09-04 15:57:08 -0500

    fix

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 mongoose
-    .connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/movies', { useNewUrlParser: true })
+    .connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true, useUnifiedTopology: true} )
     .catch(e => {
         console.error('Connection error', e.message)
     
