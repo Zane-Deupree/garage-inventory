c8f1253832cb807eeb46eb583277585ec750fc1e
Zane Deupree <zdtexan@gmail.com>
2020-09-04 17:07:31 -0500

    hopeful

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
+
+const app = express()
+const PORT = process.argv.PORT || 8070
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/api', movieRouter)
+app.use("*", function(req, res){
+    res.sendFile("./client/build/index.html")
+  })
+  app.use(express.static("public"))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 const MongoClient = require('mongodb').MongoClient;
 
 // replace the uri string with your connection string.
